0d30082d47ee37728731624b45baa494cf05ed9c
gx <1157596306@qq.com>
2020-08-28 16:58:42 +0800

    1.修复；

---
+++ addlibrary/src/main/java/com/goldhouse/addlibrary/MyUtils.java
@@ -0,0 +1,14 @@
+package com.goldhouse.addlibrary;
+
+/**
+ * Description:
+ * Author: GX
+ * CreateDate: 2020/8/28 16:34
+ * Version: 1.0
+ */
+public class MyUtils {
+
+    public static int add(int a, int b) {
+        return a+b;
+    }
+}
